b81bc6bb7d62a775c4b5371b435d543787728c3e
tom_glb <1932759573@qq.com>
2021-07-31 12:49:14 +0800

    前、中、后序查找

--- src/com/yhl/tree/BinaryTreeDemo.java
+++ src/com/yhl/tree/BinaryTreeDemo.java
@@ -22,10 +22,12 @@ public class BinaryTreeDemo {
         HeroNode h1 = new HeroNode(2, "吴用");
         HeroNode h2 = new HeroNode(3, "卢俊义");
         HeroNode h3 = new HeroNode(4, "林冲");
+        HeroNode h4 = new HeroNode(5, "关胜");
 
         root.setLeft(h1);
         root.setRight(h2);
         h2.setRight(h3);
+        h2.setLeft(h4);
 
         binaryTree.setRoot(root);
 
@@ -36,8 +38,13 @@ public class BinaryTreeDemo {
 //        binaryTree.infixOrder();//2、1、3、4
 
         //后序遍历
-        binaryTree.postOrder();//2、4、3、1
+//        binaryTree.postOrder();//2、4、3、1
 
+        //比较次数
+        HeroNode result = binaryTree.preOrderSearch(5);//4
+//        HeroNode result = binaryTree.infixOrderSearch(5);//3
+//        HeroNode result = binaryTree.postOrderSearch(5);//2
+        System.out.println(result);
 
 
     }
@@ -61,6 +68,34 @@ class BinaryTree{
 
     public void setRoot(HeroNode root) {
         this.root = root;
+    }
+
+    //前序查找
+    public HeroNode preOrderSearch(int no){
+        //如果树不为空
+        if (root != null){
+            return root.preOrderSearch(no);
+        }else {
+            return null;
+        }
+    }
+
+    //中序查找
+    public HeroNode infixOrderSearch(int no){
+        if (root != null){
+            return root.infixOrderSearch(no);
+        }else {
+            return null;
+        }
+    }
+
+    //后序查找
+    public HeroNode postOrderSearch(int no){
+        if (root != null){
+            return root.postOrderSearch(no);
+        }else {
+            return null;
+        }
     }
 
     //前序遍历
@@ -142,6 +177,91 @@ class HeroNode{
                 "no=" + no +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    /**
+     * 前序查找
+     * @param no 待查找的员工的编号
+     * @return 如果找到，返回该员工，没有找到返回null
+     */
+    public HeroNode preOrderSearch(int no){
+        System.out.println("前序查找");
+        HeroNode res = null;
+        //当前结点是否为目标值
+        if (this.no == no){
+            return this;
+        }
+
+        //向左递归前序查找
+        if (this.left != null){
+            res = this.left.preOrderSearch(no);
+        }
+        //判断向左递归是否找到
+        if(res != null){
+            return res;
+        }
+
+        //向右递归前序查找
+        if (this.right != null){
+            res = this.right.preOrderSearch(no);
+        }
+
+        return res;
+    }
+
+    //中序查找
+    public HeroNode infixOrderSearch(int no){
+        HeroNode res = null;
+        //向左递归中序查找
+        if (this.left != null){
+            res = this.left.infixOrderSearch(no);
+        }
+        if (res != null){
+            return res;
+        }
+
+        System.out.println("中序查找");
+        //判断当前结点是否为目标结点
+        if (this.no == no){
+            return this;
+        }
+
+        //向右递归中序查找
+        if (this.right != null){
+            res = this.right.infixOrderSearch(no);
+        }
+        return res;
+    }
+
+    //后序查找
+    public HeroNode postOrderSearch(int no){
+
+        HeroNode res = null;
+        //向左递归后序查找
+        if (this.left != null){
+            res = this.left.postOrderSearch(no);
+        }
+
+        if (res != null){
+            return res;
+        }
+
+        //向右递归后序查找
+        if (this.right != null){
+            res = this.right.postOrderSearch(no);
+        }
+
+        if (res != null){
+            return res;
+        }
+
+        System.out.println("后序查找");
+        //当前结点是否为目标值
+        if (this.no == no){
+            return this;
+        }
+
+        return res;
     }
 
     //前序遍历
